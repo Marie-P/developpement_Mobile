d3a85b0a89563f555d68d323490125289242aa1e
Marie-P <mpbwelle@gmail.com>
2020-12-25 17:14:12 +0100

    minify

--- js/TRex/utils/cactus.js
+++
@@ -1,8 +0,0 @@
-class Cactus {
-  constructor(posX, posY) {
-    this.sprite = new Image();
-    this.sprite.src = Math.floor(Math.random() * 2) ? "./assets/3.3_Chrome_T_Rex/cactus1.png" : "./assets/3.3_Chrome_T_Rex/cactus2.png"
-    this.posX = posX;
-    this.posY = posY;
-  }
-}
--- js/TRex/utils/cloud.js
+++
@@ -1,8 +0,0 @@
-class Cloud {
-  constructor(posX, posY) {
-    this.sprite = new Image();
-    this.sprite.src ="./assets/3.3_Chrome_T_Rex/cloud.png";
-    this.posX = posX;
-    this.posY = posY;
-  }
-}
