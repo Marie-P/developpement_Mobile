83f8e573a0120a1c37d81f575eee5d8221581052
Marie-P <mpbwelle@gmail.com>
2020-12-25 20:25:24 +0100

    project with menu

--- III.Canvas/3.3_Chrome_T_Rex/js/source/utils/cactus.js
+++
@@ -1,8 +0,0 @@
-class Cactus {
-  constructor(posX, posY) {
-    this.sprite = new Image();
-    this.sprite.src = Math.floor(Math.random() * 2) ? "./assets/cactus1.png" : "./assets/cactus2.png"
-    this.posX = posX;
-    this.posY = posY;
-  }
-}
--- III.Canvas/3.3_Chrome_T_Rex/js/source/utils/cloud.js
+++
@@ -1,8 +0,0 @@
-class Cloud {
-  constructor(posX, posY) {
-    this.sprite = new Image();
-    this.sprite.src ="./assets/cloud.png";
-    this.posX = posX;
-    this.posY = posY;
-  }
-}
